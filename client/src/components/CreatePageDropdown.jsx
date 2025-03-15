f984d13ec89d7a0db9fc5e0cdb77fdf1a0014f46
suhana <suhanakaura01@gmail.com>
2025-03-15 14:19:28 +0530

    Structure created

---
+++ client/src/components/CreatePageDropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePageDropdown = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreatePageDropdown
---
+++ server/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
+export const connectDB = () => {
+   dotenv.config();
+   mongoose.connect(process.env.MONGODB_URL, {
+       useNewUrlParser:true,
+       useUnifiedTopology:true
+   })
+   .then(() => console.log('connected to mongo'))
+   .catch((err)=> {
+       console.error('failed to connect with mongo');
+       console.error(err);
+   })
+}
